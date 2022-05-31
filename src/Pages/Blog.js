fb22ea1e955dc7974cce1bc903749e6de2c86f70
NazarNech <nazar.nechitailo@gmail.com>
2022-06-01 00:04:15 +0300

    add content to blog and home

--- src/Pages/Blog.js
+++ src/Pages/Blog.js
@@ -1,8 +1,85 @@
 import React, { Component } from "react";
-
+import { Container, Col, Row, Card, ListGroup } from "react-bootstrap";
 class Blog extends Component {
   render() {
-    return <div style={{ marginTop: 100 }}>Blog</div>;
+    return (
+      <div style={{ marginTop: 100 }}>
+        <Row>
+          <Col md="9">
+            <div className="d-flex align-items-center me-5">
+              <div className="flex-shrink-0">
+                <img
+                  width={150}
+                  height={150}
+                  className="mr-3"
+                  src="https://emgotas.files.wordpress.com/2016/11/what-is-a-team.jpg"
+                  alt="photo"
+                />
+              </div>
+              <div className="flex-grow-1 ms-3">
+                <h5>Blog post</h5>
+                <p>Lorem Ipsum</p>
+              </div>
+            </div>
+
+            <div className="d-flex align-items-center me-5">
+              <div className="flex-shrink-0">
+                <img
+                  width={150}
+                  height={150}
+                  className="mr-3"
+                  src="https://emgotas.files.wordpress.com/2016/11/what-is-a-team.jpg"
+                  alt="photo"
+                />
+              </div>
+              <div className="flex-grow-1 ms-3">
+                <h5>Blog post</h5>
+                <p>Lorem Ipsum</p>
+              </div>
+            </div>
+
+            <div className="d-flex align-items-center me-5">
+              <div className="flex-shrink-0">
+                <img
+                  width={150}
+                  height={150}
+                  className="mr-3"
+                  src="https://emgotas.files.wordpress.com/2016/11/what-is-a-team.jpg"
+                  alt="photo"
+                />
+              </div>
+              <div className="flex-grow-1 ms-3">
+                <h5>Blog post</h5>
+                <p>Lorem Ipsum</p>
+              </div>
+            </div>
+          </Col>
+
+          <Col md="3">
+            <h5 className="text-center mt-5">Categories</h5>
+
+            <Card>
+              <ListGroup as="ul" variant="flush" className="list-group">
+                <ListGroup.Item as="li" className="list-group-item" active>
+                  category 1
+                </ListGroup.Item>
+                <ListGroup.Item>category 2</ListGroup.Item>
+                <ListGroup.Item>category 3</ListGroup.Item>
+                <ListGroup.Item>category 4</ListGroup.Item>
+                <ListGroup.Item>category 5</ListGroup.Item>
+              </ListGroup>
+            </Card>
+
+            <Card className="mt-3 bg-light">
+              <Card.Body>
+                <Card.Title>Slide Widget</Card.Title>
+                <Card.Text>Lorem Ipsum</Card.Text>
+              </Card.Body>
+            </Card>
+          </Col>
+        </Row>
+      </div>
+    );
   }
 }
 
--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,8 +1,58 @@
 import React, { Component } from "react";
+import { Card, CardImg, Container, Button } from "react-bootstrap";
 
 class Home extends Component {
   render() {
-    return <div>Home</div>;
+    return (
+      <div style={{ marginTop: 100 }}>
+        <Container>
+          <h2 className="text-center m-4">Our team</h2>
+          <div className="row">
+            <div className="col">
+              <Card className="m-4 text-center" bg="light" border="primary">
+                <Card.Img
+                  variant="top"
+                  src="https://potomac.edu/wp-content/uploads/2020/12/benefits-of-coding-e1606911064541.jpg"
+                />
+                <Card.Body>
+                  <Card.Title>Developers</Card.Title>
+                  <Card.Text>Team 1</Card.Text>
+                  <Button variant="primary">About team</Button>
+                </Card.Body>
+              </Card>
+            </div>
+
+            <div className="col">
+              <Card className="m-4 text-center" bg="light" border="primary">
+                <Card.Img
+                  variant="top"
+                  src="https://static-ssl.businessinsider.com/image/534bf90369bedd4c50730c6d-1200-800/ap110816116476.jp2"
+                />
+                <Card.Body>
+                  <Card.Title>QA Engineers</Card.Title>
+                  <Card.Text>Team 2</Card.Text>
+                  <Button variant="primary">About team</Button>
+                </Card.Body>
+              </Card>
+            </div>
+
+            <div className="col">
+              <Card className="m-4 text-center" bg="light" border="primary">
+                <Card.Img
+                  variant="top"
+                  src="https://i0.wp.com/humantouchtranslations.com/wp-content/uploads/2018/08/project-management.jpg?fit=1200%2C800&ssl=1"
+                />
+                <Card.Body>
+                  <Card.Title>Project Managers</Card.Title>
+                  <Card.Text>Team 3</Card.Text>
+                  <Button variant="primary">About team</Button>
+                </Card.Body>
+              </Card>
+            </div>
+          </div>
+        </Container>
+      </div>
+    );
   }
 }
 
